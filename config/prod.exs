debb45406e37aed719954893b7c5a9be85f2d275
Antonin Carette <antonin.carette@gmail.com>
2016-10-10 12:05:01 +0200

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :simple_todo_app, SimpleTodoApp.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "https://radiant-bastion-51881.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwared_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
