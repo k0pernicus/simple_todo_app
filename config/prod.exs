b3b6ea984a70dfd469f6705f7dc15d0b22a9c77f
Antonin Carette <antonin.carette@gmail.com>
2016-10-10 11:44:22 +0200

    Refactoring code

--- config/prod.exs
+++ config/prod.exs
@@ -18,15 +18,15 @@ config :simple_todo_app, SimpleTodoApp.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
+# Do not print debug messages in production
+config :logger, level: :info
+
 # Configuration of your database
 config :simple_todo_app, SimpleTodoApp.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
-
-# Do not print debug messages in production
-config :logger, level: :info
 
 # ## SSL Support
 #
